b2a87b21339ee49c349e3e0bd1fdcc359231e501
Thanyanit <46578644+Thanyanit-J@users.noreply.github.com>
2023-04-09 05:48:33 +0700

    🔧 Update: astro config base repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tunplace.com',
-  base: '/',
+  base: '/tunplace.com',
 })
