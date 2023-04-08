2495757a74d2f79f331ac7022a90dd2f75b67e11
Thanyanit <46578644+Thanyanit-J@users.noreply.github.com>
2023-04-09 06:44:18 +0700

    ➕ Add dependency: Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tunplace.com',
   base: '/tunplace.com',
-})
+  integrations: [svelte()]
+});
